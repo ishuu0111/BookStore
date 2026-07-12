96ee651ad83e74f0342fb7f44feee60dd2fe9aae
ishuu0111 <aishwarya01112006@gmail.com>
2026-07-12 10:09:09 +0530

    changes2

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 import app from './app.js';
 import { connectToDatabase } from './db.js';
-
 const PORT = Number(process.env.PORT) || 5000;
-
 const startServer = async () => {
     await connectToDatabase();
     console.log('MongoDB connected');
-
     app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
     });
 };
-
 startServer().catch(err => {
     console.error('Failed to start server:', err.message);
     process.exit(1);
